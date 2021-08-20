02faaaa5222707030dde757df6bbc3e2e03d897b
Diego Napoli <37269543+d-napoli@users.noreply.github.com>
2021-08-20 18:55:46 -0300

    2 - Bubble Sort
    Bubble Sort algorithm in Python

--- 2-Bubble-Sort/main.py
+++ 2-Bubble-Sort/main.py
@@ -4,24 +4,28 @@ class BubbleSort():
         pass
 
     def sorting(self):
-        array_size = len(self.vector)
+        array_size = len(self.vector) # what is the size of the provided array?
         k = 0
 
-        got_in = True
+        got_in = True # variable control, if this is False after the end of the outside loop, it means that we have sorted everything already
 
         while k < array_size - 1 and got_in:
             j = 0
+
+            # the max of executions for each loop depends on how much we have sorted the array yet
+            # once the bigger numbers are already in the end
+            # we don't need to go all the way to the end, this variable makes the algorithm optimizer, since it will have less executions
             max_exec = array_size - 1 if k == 0 else array_size - (k + 1)
-            while j < max_exec:
-                if self.vector[j] > self.vector[j + 1]:
-                    self.vector[j], self.vector[j + 1] = self.vector[j + 1], self.vector[j]
+            while j < max_exec: # while j is less than the max of executions for this loop
+                if self.vector[j] > self.vector[j + 1]: # if the curr number is bigger than the next one
+                    self.vector[j], self.vector[j + 1] = self.vector[j + 1], self.vector[j] # swap the array numbers
                     got_in = True
                 j += 1
 
             got_in = False if not got_in else got_in
             k += 1
 
-        return self.vector
+        return self.vector # return the vector
 
-b = BubbleSort()
+b = BubbleSort([10, 11, 12, 13, 14, 2, 232, 100, 1, 0, 101])
 print(b.sorting())
